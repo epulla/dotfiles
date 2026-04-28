7eb43a03b4dc2a154db7bbb614c0797db2909b51
epulla <erickpulladsi@gmail.com>
2026-04-27 20:25:09 -0500

    feat: add color_scheme to wezterm tabline

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,8 +1,10 @@
 local wezterm = require("wezterm")
 local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
 
+local color_scheme = "Catppuccin Mocha"
 local config = {
 	text_background_opacity = 1.0,
+	-- color_scheme = color_scheme,
 	keys = {
 		-- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
 		{ key = "LeftArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
@@ -24,7 +26,7 @@ local config = {
 tabline.setup({
 	options = {
 		icons_enabled = true,
-		theme = "Catppuccin Mocha",
+		theme = color_scheme,
 	},
 })
 
