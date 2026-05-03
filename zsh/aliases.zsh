e1fba5a944ea996f569228731b8b78c9b1940bd4
epulla <erickpulladsi@gmail.com>
2026-05-02 22:25:15 -0500

    feat: add zsh config

---
+++ zsh/aliases.zsh
@@ -0,0 +1,3 @@
+alias gce='git config --global user.name epulla && git config --global user.email erickpulladsi@gmail.com && echo "Updated to erickpulladsi@gmail.com creds"'
+alias gchm='git config --global user.name epulla-hm && git config --global user.email epulla@happymoney.com && echo "Updated to epulla@happymoney.com creds"'
+
