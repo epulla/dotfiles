6e56a807669bde353bf94dcbaea7f732f0248bda
epulla <erickpulladsi@gmail.com>
2026-05-04 16:04:21 -0500

    feat: add autosave on edit functionality

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,19 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
+  group = vim.api.nvim_create_augroup("autosave", { clear = true }),
+  callback = function(args)
+    local buf = args.buf
+    if vim.bo[buf].buftype ~= "" or not vim.bo[buf].modifiable or not vim.bo[buf].modified then
+      return
+    end
+    if vim.api.nvim_buf_get_name(buf) == "" then
+      return
+    end
+    vim.api.nvim_buf_call(buf, function()
+      vim.cmd("silent! write")
+    end)
+  end,
+})
