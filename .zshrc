a7c227ec7f71fa22290b92a789f95861045e1d86
epulla <erickpulladsi@gmail.com>
2026-05-06 11:14:27 -0500

    refactor: move aliases to a separate file

--- .zshrc
+++ .zshrc
@@ -99,33 +99,6 @@ source $ZSH/oh-my-zsh.sh
 # Compilation flags
 # export ARCHFLAGS="-arch x86_64"
 
-# Set personal aliases, overriding those provided by oh-my-zsh libs,
-# plugins, and themes. Aliases can be placed here, though oh-my-zsh
-# users are encouraged to define aliases within the ZSH_CUSTOM folder.
-# For a full list of active aliases, run `alias`.
-#
-# Example aliases
-# alias zshconfig="mate ~/.zshrc"
-# alias ohmyzsh="mate ~/.oh-my-zsh"
-
-# neovim alias
-alias vim="nvim"
-
-# Docker CLI Aliases
-alias dc="docker-compose"
-alias dl="aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 730502903637.dkr.ecr.us-east-1.amazonaws.com"
-alias dp="docker system prune -f --volumes"
-
-# Airflow2 Aliases
-alias dx2="docker exec -it airflow2"
-alias dx2-test="docker exec -e AIRFLOW_ENV=test -it airflow2"
-
-# utilities
-## count files in a directory
-alias file-count="ls -1q | wc -l"
-
-# tmux
-alias tmux="tmux -2"
 
 # pyenv
 export PYENV_ROOT="$HOME/.pyenv"
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,32 @@
-alias gce='git config --global user.name epulla && git config --global user.email erickpulladsi@gmail.com && echo "Updated to erickpulladsi@gmail.com creds"'
-alias gchm='git config --global user.name epulla-hm && git config --global user.email epulla@happymoney.com && echo "Updated to epulla@happymoney.com creds"'
+# Set personal aliases, overriding those provided by oh-my-zsh libs,
+# plugins, and themes. Aliases can be placed here, though oh-my-zsh
+# users are encouraged to define aliases within the ZSH_CUSTOM folder.
+# For a full list of active aliases, run `alias`.
+#
+# Example aliases
+# alias zshconfig="mate ~/.zshrc"
+# alias ohmyzsh="mate ~/.oh-my-zsh"
+# git aliases
+#
+alias gce="git config --global user.name epulla && git config --global user.email erickpulladsi@gmail.com"
+alias gchm="git config --global user.name epulla-hm && git config --global user.email epulla@happymoney.com"
+
+# neovim alias
+alias vim="nvim"
+
+# Docker CLI Aliases
+alias dc="docker-compose"
+alias dl="aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 730502903637.dkr.ecr.us-east-1.amazonaws.com"
+alias dp="docker system prune -f --volumes"
+
+# Airflow2 Aliases
+alias dx2="docker exec -it airflow2"
+alias dx2-test="docker exec -e AIRFLOW_ENV=test -it airflow2"
+
+# utilities
+## count files in a directory
+alias file-count="ls -1q | wc -l"
+
+# tmux
+alias tmux="tmux -2"
 
