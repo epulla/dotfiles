d723826be24401ee33f44eba712b724e91390359
epulla <erickpulladsi@gmail.com>
2026-05-14 11:02:25 -0500

    feat(nvim): brighten line numbers and comments in bearded theme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -41,6 +41,26 @@ return {
       require("bearded").setup({
         flavor = "vivid-black", -- change to any flavor you want
       })
+
+      local function apply_overrides()
+        -- Brighter relative line numbers
+        vim.api.nvim_set_hl(0, "LineNr", { fg = "#9a9a9a" })
+        vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#9a9a9a" })
+        vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#9a9a9a" })
+        -- Yellow current line number for contrast
+        vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#e5c07b", bold = true })
+        -- Brighter comments
+        vim.api.nvim_set_hl(0, "Comment", { fg = "#8a8a8a", italic = true })
+        vim.api.nvim_set_hl(0, "SpecialComment", { fg = "#8a8a8a", italic = true })
+        vim.api.nvim_set_hl(0, "@comment", { fg = "#8a8a8a", italic = true })
+      end
+
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        pattern = "bearded",
+        callback = apply_overrides,
+      })
+
+      apply_overrides()
     end,
   },
   -- Configure LazyVim
